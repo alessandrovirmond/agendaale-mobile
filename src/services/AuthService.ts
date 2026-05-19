f64fe43babf6870de52276239002776900064c99
Alessandro Virmond <avirmond2000@gmail.com>
2026-05-19 17:47:33 -0300

    feat: point api to render production and update auth payload

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,4 +1,4 @@
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { GoogleSignin, User } from '@react-native-google-signin/google-signin';
 import * as SecureStore from 'expo-secure-store';
 
 const TOKEN_KEY = '@agendaale_jwt';
@@ -6,29 +6,32 @@ const TOKEN_KEY = '@agendaale_jwt';
 export const AuthService = {
   configureGoogleSignIn() {
     GoogleSignin.configure({
-      webClientId: 'AIzaSyC7sv28Wy0olSwOpnYrjcMApK-1QPZL-Fo.apps.googleusercontent.com', 
+      webClientId: '523674610292-7q9qaltbhrdrr58h0hdn134s26r36t10.apps.googleusercontent.com', 
     });
   },
 
-async signInWithGoogle(): Promise<string> {
+async signInWithGoogle(): Promise<User['user']> {
     await GoogleSignin.hasPlayServices();
     const response = await GoogleSignin.signIn();
     
-    if (response.type === 'success') {
-      if (!response.data.idToken) {
-        throw new Error('Token do Google não retornado.');
-      }
-      return response.data.idToken;
+    if (response.type === 'success' && response.data.user) {
+      return response.data.user;
     } else {
       throw new Error('Login cancelado ou mal sucedido.');
     }
   },
 
-  async authenticateWithApi(googleIdToken: string): Promise<string> {
-    const response = await fetch('http://192.168.X.X:5260/api/auth/google', {
+async authenticateWithApi(googleUser: User['user']): Promise<string> {
+    const payload = {
+      externalAuthId: googleUser.id,
+      email: googleUser.email,
+      name: googleUser.name
+    };
+
+    const response = await fetch('https://agendaale-api-1.onrender.com/api/auth', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ token: googleIdToken }),
+      body: JSON.stringify(payload),
     });
 
     if (!response.ok) {
